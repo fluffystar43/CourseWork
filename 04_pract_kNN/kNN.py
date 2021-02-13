b117864a738ed55e845f7b9645ccbe46f562f067
REPIRAS <kraitek.master.2014@mail.ru>
2021-02-13 11:29:18 +0300

    Практика 4 – k ближайших соседей

---
+++ 04_pract_kNN/kNN.py
@@ -0,0 +1,23 @@
+import numpy as np
+import math
+from sklearn.preprocessing import normalize
+
+def k_nearest(X, k, obj):
+    sub_X = X[:, 0: -1]
+    new = np.zeros((17, 2))
+    new[:16, :2] = sub_X
+    new[16] = obj
+    new = normalize(new, axis=0, norm='max')
+    for i in range(sub_X.shape[0] - 1):
+        sub_X[i] = new[i]
+    obj = new[16]
+    a = [dist(i, obj) for i in sub_X]
+    b = np.argsort(a)
+    b = b[0:k]
+    nearest_classes = X[[b], -1]
+    unique, counts = np.unique(nearest_classes, return_counts = True)
+    object_class = unique[np.argmax(counts)]
+    return object_class
+
+def dist(p1, p2):
+    return math.sqrt(sum((p1 - p2)**2))
