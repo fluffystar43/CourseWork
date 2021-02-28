34fd387a68de706a94cef8aa7ef1b288acb961fb
REPIRAS <kraitek.master.2014@mail.ru>
2021-02-28 15:17:55 +0300

    Все замечания исправлены

--- 04_pract_kNN/kNN.py
+++ 04_pract_kNN/kNN.py
@@ -1,17 +1,18 @@
 import numpy as np
 import math
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import StandardScaler
 
 def k_nearest(X, k, obj):
+    scaler = StandardScaler()
     sub_X = X[:, 0: -1]
-    new = np.zeros((17, 2))
-    new[:16, :2] = sub_X
-    new[16] = obj
-    new = normalize(new, axis=0, norm='max')
-    for i in range(sub_X.shape[0] - 1):
-        sub_X[i] = new[i]
-    obj = new[16]
-    distance = [dist(i, obj) for i in sub_X]
+    scaler.fit(sub_X)
+    average = scaler.mean_
+    standard_deviation = np.std(sub_X, axis = 0)
+    for i in range(len(obj)):
+        obj[i] = (obj[i]-(np.mean(sub_X, axis=0))[i])/(np.std(sub_X, axis=0))[i]
+    for i in range(sub_X.shape[1]):
+        sub_X[:, i] = (sub_X[:, i] - average[i])/standard_deviation[i]
+    distance = [euclidean_distance(i, obj) for i in sub_X]
     sorting = np.argsort(distance)
     sortingMas = sorting[0:k]
     nearest_classes = X[[sortingMas], -1]
@@ -19,5 +20,5 @@ def k_nearest(X, k, obj):
     object_class = unique[np.argmax(counts)]
     return object_class
 
-def dist(p1, p2):
+def euclidean_distance(p1, p2):
     return math.sqrt(sum((p1 - p2)**2))
